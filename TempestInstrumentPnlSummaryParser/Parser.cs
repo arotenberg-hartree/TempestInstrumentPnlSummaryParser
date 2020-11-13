80ebc2b9b8c72d185c79663e076ef82939d2126c
Alan Rotenberg <arotenberg@hartreepartners.com>
2020-11-12 21:15:20 -0500

    locating the excel files

--- TempestInstrumentPnlSummaryParser/Parser.cs
+++ TempestInstrumentPnlSummaryParser/Parser.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace TempestInstrumentPnlSummaryParser
 {
@@ -16,10 +19,35 @@ namespace TempestInstrumentPnlSummaryParser
         }
         public void Run(DateTime startDate, DateTime endDate)
         {
-            logger.LogInformation("Running from base folder: {baseFolder}", settings.Value.ReportBaseFolder);
+            logger.LogInformation("Running with settings: {settings}", JsonConvert.SerializeObject(settings.Value));
             logger.LogInformation("Start Date: {startDate:yyyy-MM-dd}", startDate);
             logger.LogInformation("End Date: {endDate:yyyy-MM-dd}", endDate);
+
+            var files = GetFiles(startDate, endDate);
+
             
+        }
+
+        private List<(string, DateTime)> GetFiles(DateTime startDate, DateTime endDate)
+        {
+            logger.LogInformation("Locating files...");
+            var entries = Directory.GetFileSystemEntries(settings.Value.ReportBaseFolder, settings.Value.FileSearchPattern, SearchOption.AllDirectories);
+            var result = new List<(string, DateTime)>();
+            foreach(var entry in entries)
+            {
+                var fi = new FileInfo(entry);
+                var name = fi.Name;
+                foreach(var r in settings.Value.FileNameStringsToReplace) name = name.Replace(r, string.Empty);
+                if (!DateTime.TryParseExact(name, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out DateTime d)) continue;
+                if (d < startDate) continue;
+                if (d > endDate) continue;
+
+                logger.LogInformation("Located file for {d:yyyy-MM-dd}: {entry}", d, entry);
+                result.Add((entry, d));
+            }
+            
+            logger.LogInformation("Located {count} files...", result.Count);
+            return result;
         }
     }
 }
--- TempestInstrumentPnlSummaryParser/Settings.cs
+++ TempestInstrumentPnlSummaryParser/Settings.cs
@@ -3,5 +3,7 @@ namespace TempestInstrumentPnlSummaryParser
     public class Settings
     {
         public string ReportBaseFolder { get; set; }
+        public string FileSearchPattern { get; set; }
+        public string[] FileNameStringsToReplace { get; set; }
     }
 }
